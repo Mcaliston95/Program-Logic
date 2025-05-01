3f5e77f40ce4067bd2295d2caa5b928447a5374e
Mcaliston95 <mcalistonlewis@gmail.com>
2025-04-30 21:59:55 -0400

    change vari

---
+++ Assign2.3.py
@@ -0,0 +1,14 @@
+itemname = "TV stand"
+retailprice = 325.00
+wholesaleprice = 200.00
+
+profit = retailprice - wholesaleprice
+salePrice = retailprice - ( retailprice * 0.25)
+salesprofit = salePrice - wholesaleprice
+
+print("Itemname:",itemname)
+print("retailprice",retailprice)
+print("wholesaleprice",wholesaleprice)
+print("profit",profit)
+print("sales",salePrice)
+print("sales",salesprofit)
